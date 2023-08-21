86fd7e2b7a5731ba35b6adc5fed7c156a1ece914
VIVEK CHAVAN <vivek27498@gmail.com>
2023-08-21 20:38:54 +0530

    Update orderbook_binance.py
    removed keys

--- orderbook_binance.py
+++ orderbook_binance.py
@@ -8,8 +8,8 @@ from  database_connector import DatabaseConnector
 from logger import setup_logger
 
 from binance.client import Client
-user_key = '57730420ff758f4b3efa3f0d5b86a2083103816eb5982808e32f0c1f2663bf5f'
-secret_key = '5270cea3b14bd90260a46ed726e061fae1dfcc677d75bbfa1a79f4d8ab6aefea'
+user_key = '577304f5f'
+secret_key = '5270caefea'
 binance_client = Client(user_key, secret_key,base_endpoint='https://testnet.binancefuture.com/fapi/v1/order',tld='com',testnet=True)
 
 
